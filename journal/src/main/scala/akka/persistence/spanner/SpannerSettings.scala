2b9a85d370b8e1e0c8d41a39e296aa136d96da4f
Johan Andrén <johan@markatta.com>
2021-06-28 17:21:30 +0200

    Allow messages to actually be the max size for spanner (#137)
    * Allow messages to actually be the max size for spanner (Netty client limits to 4mb by default)
    * Incorporating some internal stable API changes (from the previous PR #106)

--- journal/src/main/scala/akka/persistence/spanner/SpannerSettings.scala
+++ journal/src/main/scala/akka/persistence/spanner/SpannerSettings.scala
@@ -4,18 +4,18 @@
 
 package akka.persistence.spanner
 
-import akka.annotation.InternalApi
+import akka.annotation.InternalStableApi
 import akka.persistence.spanner.SpannerSettings.SessionPoolSettings
-import com.typesafe.config.Config
 import akka.util.JavaDurationConverters._
+import com.typesafe.config.Config
 
 import scala.concurrent.duration.FiniteDuration
 
 /**
  * INTERNAL API
  */
-@InternalApi
-private[spanner] object SpannerSettings {
+@InternalStableApi
+object SpannerSettings {
   final class SessionPoolSettings(config: Config) {
     val maxSize = config.getInt("max-size")
     // Spanner only supports 100 sessions per gRPC channel. We'd need multiple channels to support
@@ -37,15 +37,19 @@ private[spanner] object SpannerSettings {
   }
 }
 
-private[spanner] final class QuerySettings(config: Config) {
+/**
+ * INTERNAL API
+ */
+@InternalStableApi
+final class QuerySettings(config: Config) {
   val refreshInterval: FiniteDuration = config.getDuration("refresh-interval").asScala
 }
 
 /**
  * INTERNAL API
  */
-@InternalApi
-private[spanner] final class SpannerSettings(config: Config) {
+@InternalStableApi
+final class SpannerSettings(config: Config) {
   val project = config.getString("project")
   val instance = config.getString("instance")
   val database = config.getString("database")
